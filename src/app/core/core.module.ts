7aa572811db1335d45e6938580b9e0c0a08e6cb9
bmichaux <bmichaux@altissia.com>
2018-09-18 13:08:08 +0200

    add core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,23 @@
+import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: []
+})
+export class CoreModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: []
+    };
+  }
+
+  // Avoid instantiation of multiple core module
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
